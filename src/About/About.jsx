91ce7f413ec74f7a61bddfc5dee31c6f5adbc58f
suryamsp <murugesansuryamsp@gmail.com>
2023-10-10 07:14:49 -0700

    resume

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -13,8 +13,8 @@ export function About({sty}) {
         <p className='titl2'style={sty}>WHY HIRE ME FOR YOUR NEXT PROJECT</p>
         <p className='titl3'style={sty}>Iam 25 years old creative <label>SOFTWARE DEVELOPER</label>. I bulid clean, appealing, and function interfaces which comply with the latest web standard </p>
         <a className='btns'
-          href="https://drive.google.com/drive/my-drive"
-          download="surya_resume.pdf"
+          href="./img/surya_resume.pdf"
+          download
           target="_blank"
         ><p className='btn_name' >DOWNLOAD CV </p><DownloadIcon /></a>
       </div>
